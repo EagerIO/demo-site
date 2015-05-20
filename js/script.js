d925f54fb30b41a293f081bb98c871b8084b18ac
Adam Schwartz <aschwartz@hubspot.com>
2015-05-19 20:17:35 -0400

    Add Eager banner, note, and toggle button to all pages when viewed not in an iFrame

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-var getParams, ready;
-
-getParams = function() {
-  var index, param, paramBits, params, _i, _len;
-  paramBits = document.location.search.substring(1).split('&');
-  params = {};
-  for (_i = 0, _len = paramBits.length; _i < _len; _i++) {
-    param = paramBits[_i];
-    index = param.indexOf('=');
-    params[param.substring(0, index)] = decodeURIComponent(param.substring(index + 1));
-  }
-  return params;
-};
+var ready, addBanner, addOffButton, addOnButton, addNote;
 
 ready = function(fn) {
   if (document.readyState != 'loading') {
@@ -18,12 +6,54 @@ ready = function(fn) {
   } else {
     document.addEventListener('DOMContentLoaded', fn)
   }
-}
+};
+
+addBanner = function() {
+  var banner = document.createElement('a');
+  banner.className = 'eager-banner';
+  banner.setAttribute('href', 'https://eager.io/example-site');
+  document.body.appendChild(banner);
+};
+
+addOffButton = function() {
+  var offButton = document.createElement('a');
+  offButton.className = 'eager-off-button';
+  offButton.setAttribute('href', location.href.match('photonphotos.github.io') ? location.href.replace('/photonphotos.github.io', '/photonphotos.github.io/without-eager') : location.href.replace('/photon.photos', '/photon.photos/without-eager'));
+  document.body.appendChild(offButton);
+};
+
+addOnButton = function() {
+  var onButton = document.createElement('a');
+  onButton.className = 'eager-on-button';
+  onButton.setAttribute('href', location.href.replace(/\/without\-eager/i, ''));
+  document.body.appendChild(onButton);
+};
+
+addNote = function() {
+  var note = document.createElement('div');
+  note.className = 'eager-note';
+  document.body.appendChild(note);
+};
 
 ready(function(){
-  if (getParams().eager) {
-    Array.prototype.forEach.call(document.querySelectorAll('a[href]'), function(link){
-      link.href = link.href + '?eager=true'
-    });
+  var withEager, inIframe;
+
+  withEager = !!!location.href.match('/without-eager');
+  inIframe = window.top !== window;
+
+  if (withEager) {
+    document.documentElement.className += ' with-eager';
+  } else {
+    document.documentElement.className += ' without-eager';
+  }
+
+  if (!inIframe) {
+    if (withEager) {
+      addBanner();
+      addOffButton();
+    } else {
+      addNote();
+      addOnButton();
+    }
   }
 });
