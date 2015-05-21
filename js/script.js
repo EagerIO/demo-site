8235a2d94f595ffcf7319d0280c1bf6c5d1ac413
Adam Schwartz <aschwartz@hubspot.com>
2015-05-20 20:30:55 -0400

    Respond to demo postMessages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var ready, addBanner, addOffButton, addOnButton, addNote;
+var ready, addBanner, addOffButton, addOnButton, addNote, apps, play, pathsEqual, ensurePath;
 
 ready = function(fn) {
   if (document.readyState != 'loading') {
@@ -57,3 +57,177 @@ ready(function(){
     }
   }
 });
+
+window.addEventListener('message', function(event) {
+  if (event.data && event.data.type === 'photon-photos-demo:play') {
+    play(event.data.appId);
+  }
+}, false);
+
+apps = [
+  {
+    id: 'DvuKIoU8iTOt',
+    alias: 'photoswipe',
+    title: 'PhotoSwipe',
+    metadata: {
+      icon: {
+        key: 'hIJSQ6fSVCcOXyZ8oye9_photo-swipe-icon.png'
+      }
+    },
+    demoPath: '/photography/'
+  },
+  {
+    id: 'fNjUnjurPXkD',
+    alias: 'gumroad',
+    title: 'Gumroad',
+    metadata: {
+      icon: {
+        key: 'mwmGjbgMR6i7BJjysBWL_gumroad.png'
+      }
+    },
+    demoPath: '/photography/'
+  },
+  {
+    id: 'w-B2nEFkVIx7',
+    alias: 'social-icons',
+    title: 'Social Icons',
+    metadata: {
+      icon: {
+        key: '3qpSgatQByxdP40iveDw_social-icons-icon.png'
+      }
+    },
+    demoPath: '/photography/'
+  },
+  {
+    id: 'eA9ULux0UOJP',
+    alias: 'smartunderline',
+    title: 'SmartUnderline',
+    metadata: {
+      icon: {
+        key: 'vG072c9WT7qrCgEucqcz_smart-underline-icon.png'
+      }
+    },
+    demoPath: '/photography/'
+  },
+  {
+    id: 'BurROp-cWQ3Y',
+    alias: 'like-button',
+    title: 'Like Button',
+    metadata: {
+      icon: {
+        key: '0AoPcpqqSRWvPRwQJugR_like-button-icon.png'
+      }
+    },
+    demoPath: '/blog/thailand/'
+  },
+  {
+    id: '8KqAj3D-rZkN',
+    alias: 'disqus',
+    title: 'Disqus',
+    metadata: {
+      icon: {
+        key: '9DgPMbQaQzcwljSEAaqQ_disqus-icon.png'
+      }
+    },
+    demoPath: '/blog/thailand/'
+  },
+  {
+    id: 'z1o4cnLQKenU',
+    alias: 'typist',
+    title: 'Typist',
+    metadata: {
+      icon: {
+        key: '6Q8q8IjpSKVV34BAKmZU_typist-icon.png'
+      }
+    },
+    demoPath: '/about/'
+  },
+  {
+    id: 'AN_LdP2tOk0f',
+    alias: 'hover',
+    title: 'Hover',
+    metadata: {
+      icon: {
+        key: 'ITZqwibSPa4EiVS3AaK9_hover-css-icon.png'
+      }
+    },
+    demoPath: '/photography/'
+  },
+  {
+    id: 'PoIrgiLyzYcP',
+    alias: 'animate',
+    title: 'Animate',
+    metadata: {
+      icon: {
+        key: 'aPPw3TGLRtqQgUwTb5is_animte-css-icon.png'
+      }
+    },
+    demoPath: '/'
+  },
+  {
+    id: '3sa_P_ftYXnj',
+    alias: 'google-analytics',
+    title: 'Google Analytics',
+    metadata: {
+      icon: {
+        key: 'Q8uc7ysQTtiScDu0X2sG_google-analytics-icon.png'
+      }
+    },
+    demoPath: '/photography/'
+  }
+];
+
+play = function(appId) {
+  var i, app;
+
+  for (i = 0; i < apps.length; i++) {
+    if (apps[i].id === appId) {
+      app = apps[i];
+    }
+  }
+
+  if (!app) {
+    return;
+  }
+
+  ensurePath(app.demoPath);
+
+};
+
+pathsEqual = function(a, b) {
+  return a.replace(/\/$/, '') === b.replace(/\/$/, '');
+};
+
+ensurePath = function(path) {
+  var pathMatch, sanitizedPath;
+
+  pathMatch = location.href.match('photonphotos.github.io') ?
+    location.href.match(/photonphotos\.github\.io(\/.*)/) :
+    location.href.match(/photon\.photos(\/.*)/)
+  ;
+
+  if (pathMatch && pathMatch.length === 2) {
+    sanitizedPath = pathMatch[1];
+  }
+
+  if (pathsEqual(sanitizedPath, path)) {
+    return;
+  } else {
+    navigatePath(path);
+  }
+};
+
+navigatePath = function(path) {
+  var firstPartMatch, sanitizedFirstPart;
+
+  firstPartMatch = location.href.match('photonphotos.github.io') ?
+    location.href.match(/(.*\/photonphotos\.github\.io)/) :
+    location.href.match(/(.*\/photon\.photos)/)
+  ;
+
+  if (firstPartMatch && firstPartMatch.length === 2) {
+    sanitizedFirstPart = firstPartMatch[1];
+  }
+
+  location.href = sanitizedFirstPart + path;
+};
